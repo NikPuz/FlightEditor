00b136f78742a6c02d7c6bf0b474198e3ffdf68f
NikPuz <84337089+NikPuz@users.noreply.github.com>
2021-05-27 23:56:08 +0300

    Реализовал ViewModel для вкладки Станции на половину

--- FlightsEditor/ViewModel/ViewModel.cs
+++ FlightsEditor/ViewModel/ViewModel.cs
@@ -20,6 +20,7 @@ namespace MVVM
         {
             FillTrain();
             FillTypeTrain();
+            FillStation();
         }
 
         private void FillTrain()
@@ -37,11 +38,28 @@ namespace MVVM
             this.TypeTrain = q;
         }
 
+        private void FillStation()
+        {
+            var q = (from a in ctx.Station
+                     select a).ToList();
+            this.Station = q;
+        }
+
+        private List<Station> _station;
+        public List<Station> Station
+        {
+            get { return _station; }
+            set
+            {
+                _station = value;
+                NotifyPropertyChanged();
+            }
+        }
+
         private List<Train> _train;
         public List<Train> Train
         {
-            get
-            { return _train; }
+            get { return _train; }
             set
             {
                 _train = value;
@@ -52,8 +70,7 @@ namespace MVVM
         private List<string> _typeTrain;
         public List<string> TypeTrain
         {
-            get
-            { return _typeTrain; }
+            get { return _typeTrain; }
             set
             {
                 _typeTrain = value;
@@ -61,19 +78,64 @@ namespace MVVM
             }
         }
 
+        //---Вкладка Поезд
 
-        private Train _selectedTrain;
-        public Train SelectedTrain
+        private string _selectedTypeTrainAddTrain;
+        public string SelectedTypeTrainAddTrain
         {
-            get
-            { return _selectedTrain; }
+            get { return _selectedTypeTrainAddTrain; }
+            set
+            {
+                _selectedTypeTrainAddTrain = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _selectedNumberTrainAddTrain;
+        public int SelectedNumberTrainAddTrain
+        {
+            get { return _selectedNumberTrainAddTrain; }
+            set
+            {
+                _selectedNumberTrainAddTrain = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _tBAddTrain;
+        public string TBAddTrain
+        {
+            get { return _tBAddTrain; }
+            set
+            {
+                _tBAddTrain = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _cBAddTrain;
+        public string CBAddTrain
+        {
+            get { return _cBAddTrain; }
+            set
+            {
+                _cBAddTrain = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private Train _selectedTrainAddTrain;
+        public Train SelectedTrainAddTrain
+        {
+            get { return _selectedTrainAddTrain; }
             set
             {
                 if (value == null) return;
-                this.SelectedTypeTrain = value.Type;
-                this.SelectedNumberTrain = value.NumTrain;
+                this.SelectedTypeTrainAddTrain = value.Type;
+                this.SelectedNumberTrainAddTrain = value.NumTrain;
 
-                _selectedTrain = value;
+                _selectedTrainAddTrain = value;
                 NotifyPropertyChanged();
             }
         }
@@ -106,7 +168,7 @@ namespace MVVM
                 return _removeTrainCommand ??
                   (_removeTrainCommand = new RelayCommand(obj =>
                   {
-                      ctx.Train.Remove(_selectedTrain);
+                      ctx.Train.Remove(_selectedTrainAddTrain);
                       ctx.SaveChanges();
                       FillTrain();
 
@@ -123,8 +185,8 @@ namespace MVVM
                 return _editTrainCommand ??
                   (_editTrainCommand = new RelayCommand(obj =>
                   {
-                      Train train = ctx.Train.First(a => a.NumTrain == _selectedTrain.NumTrain);
-                      train.Type = _selectedTypeTrain;
+                      Train train = ctx.Train.First(a => a.NumTrain == _selectedTrainAddTrain.NumTrain);
+                      train.Type = _selectedTypeTrainAddTrain;
                       ctx.SaveChanges();
                       FillTrain();
 
@@ -133,54 +195,121 @@ namespace MVVM
             }
         }
 
-        private string _selectedTypeTrain;
-        public string SelectedTypeTrain
+        //---Вкладка Станции
+
+        private int _selectedIdStationAddStation;
+        public int SelectedIdStationAddStation
         {
-            get
-            { return _selectedTypeTrain; }
+            get { return _selectedIdStationAddStation; }
             set
             {
-                _selectedTypeTrain = value;
+                _selectedIdStationAddStation = value;
                 NotifyPropertyChanged();
             }
         }
 
-        private int _selectedNumberTrain;
-        public int SelectedNumberTrain
+        private string _selectedNameStationAddStation;
+        public string SelectedNameStationAddStation
         {
-            get
-            { return _selectedNumberTrain; }
+            get { return _selectedNameStationAddStation; }
             set
             {
-                _selectedNumberTrain = value;
+                _selectedNameStationAddStation = value;
                 NotifyPropertyChanged();
             }
         }
 
-        private string _tBAddTrain;
-        public string TBAddTrain
+        private string _tBAddStation;
+        public string TBAddStation
         {
-            get
-            { return _tBAddTrain; }
+            get { return _tBAddStation; }
             set
             {
-                _tBAddTrain = value;
+                _tBAddStation = value;
                 NotifyPropertyChanged();
             }
         }
 
-        private string _cBAddTrain;
-        public string CBAddTrain
+        private Station _selectedStationAddStation;
+        public Station SelectedStationAddStation
         {
-            get
-            { return _cBAddTrain; }
+            get { return _selectedStationAddStation; }
             set
             {
-                _cBAddTrain = value;
+                if (value == null) return;
+                this.SelectedIdStationAddStation = value.idStation;
+                this.SelectedNameStationAddStation = value.Name;
+                this.SelectedStationDistanceAddStation = value.Distance.ToList();
+
+                _selectedStationAddStation = value;
                 NotifyPropertyChanged();
             }
         }
 
+        private List<Distance> _selectedStationDistanceAddStation;
+        public List<Distance> SelectedStationDistanceAddStation
+        {
+            get { return _selectedStationDistanceAddStation; }
+            set
+            {
+                _selectedStationDistanceAddStation = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private RelayCommand _addStationCommand;
+        public RelayCommand AddStationCommand
+        {
+            get
+            {
+                return _addStationCommand ??
+                  (_addStationCommand = new RelayCommand(obj =>
+                  {
+                      Station station = new Station();
+                      station.Name = _tBAddStation;
+                      ctx.Station.Add(station);
+                      ctx.SaveChanges();
+                      FillStation();
+
+                      NotifyPropertyChanged();
+                  }));
+            }
+        }
+
+        private RelayCommand _removeStationCommand;
+        public RelayCommand RemoveStationCommand
+        {
+            get
+            {
+                return _removeStationCommand ??
+                  (_removeStationCommand = new RelayCommand(obj =>
+                  {
+                      ctx.Station.Remove(_selectedStationAddStation);
+                      ctx.SaveChanges();
+                      FillStation();
+
+                      NotifyPropertyChanged();
+                  }));
+            }
+        }
+
+        private RelayCommand _editStationCommand;
+        public RelayCommand EditStationCommand
+        {
+            get
+            {
+                return _editStationCommand ??
+                  (_editStationCommand = new RelayCommand(obj =>
+                  {
+                      Station station = ctx.Station.First(a => a.idStation == _selectedStationAddStation.idStation);
+                      station.Name = _selectedNameStationAddStation;
+                      ctx.SaveChanges();
+                      FillStation();
+
+                      NotifyPropertyChanged();
+                  }));
+            }
+        }
 
 
 
